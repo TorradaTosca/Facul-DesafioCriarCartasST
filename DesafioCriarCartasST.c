63b7672ccbbbe93fe33218d647cd4f3c5adc1ef2
TorradaTosca <marinho.jezieljunior@gmail.com>
2026-03-23 01:10:18 -0300

    Add 2 more attributes to the cards

--- DesafioCriarCartasST.c
+++ DesafioCriarCartasST.c
@@ -10,14 +10,16 @@ void clear_input_buffer(void) {
 int main() {
   char estado1 = 'A';
   char estado2 = 'A';
-  char codigoCarta1[3] = "A01";
-  char codigoCarta2[3] = "A01";
+  char codigoCarta1[4] = "A01";
+  char codigoCarta2[4] = "A01";
   char nomeCidade1[20] = "Cidade";
   char nomeCidade2[20] = "Cidade";
   int populacao1 = 0, populacao2 = 0;
   float area1 = 0, area2 = 0;
   float pib1 = 0, pib2 = 0;
   int numPontosTuristicos1 = 0, numPontosTuristicos2 = 0;
+  float densidPopulacional1 = 0, densidPopulacional2 = 0;
+  float pibCapta1 = 0, pibCapta2 = 0;
 
   printf("Iniciando cadastro de cartas...\n\n");
   printf("Cadastro da carta 1...\n");
@@ -52,6 +54,10 @@ int main() {
   scanf("%i", &numPontosTuristicos1);
   clear_input_buffer();
 
+  //Calculos das proximas propriedades da carta
+  densidPopulacional1 = (float) populacao1 / area1;
+  pibCapta1 = pib1 / (float) populacao1;
+
   printf("\n\nCarta 1 cadastrada.\n\nIniciando cadastro da carta 2...\n");
 
   printf("Insira o estado da carta (uma letra de A a H): ");
@@ -84,12 +90,16 @@ int main() {
   scanf("%i", &numPontosTuristicos2);
   clear_input_buffer();
 
+  //Calculos das proximas propriedades da carta
+  densidPopulacional2 = (float) populacao2 / area2;
+  pibCapta2 = pib2 / (float) populacao2;
+
   printf("\n\nCadastro de cartas finalizado.\nAs cartas cadastradas foram:");
   
-  printf("\n\nCarta1\nEstado: %c\nCódigo: %s\nNome da cidade: %s\nPopulação: %i\nÁrea: %.2f km quadrados\nPIB: %.2f bilhões de reais\nNúmero de pontos turísticos: %i",
-         estado1, codigoCarta1, nomeCidade1, populacao1, area1, pib1, numPontosTuristicos1);
-  printf("\n\nCarta2\nEstado: %c\nCódigo: %s\nNome da cidade: %s\nPopulação: %i\nÁrea: %.2f km quadrados\nPIB: %.2f bilhões de reais\nNúmero de pontos turísticos: %i",
-         estado2, codigoCarta2, nomeCidade2, populacao2, area2, pib2, numPontosTuristicos2);
+  printf("\n\nCarta1\nEstado: %c\nCódigo: %s\nNome da cidade: %s\nPopulação: %i\nÁrea: %.2f km quadrados\nPIB: %.2f bilhões de reais\nNúmero de pontos turísticos: %i\nDensidade populacional: %.2f hab/km\nPIB per capta: %.2f reais",
+         estado1, codigoCarta1, nomeCidade1, populacao1, area1, pib1, numPontosTuristicos1, densidPopulacional1, pibCapta1);
+  printf("\n\nCarta2\nEstado: %c\nCódigo: %s\nNome da cidade: %s\nPopulação: %i\nÁrea: %.2f km quadrados\nPIB: %.2f bilhões de reais\nNúmero de pontos turísticos: %i\nDensidade populacional: %.2f hab/km\nPIB per capta: %.2f reais",
+         estado2, codigoCarta2, nomeCidade2, populacao2, area2, pib2, numPontosTuristicos2, densidPopulacional2, pibCapta2);
 
 
   return 0;
